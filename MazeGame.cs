277198881a3419302432bb608fe9767ab6e28f2c
anonymousdoodle <123794824+anonymousdoodle@users.noreply.github.com>
2025-01-10 14:08:34 +0800

    Add files via upload

---
+++ MazeGame.cs
@@ -0,0 +1,20 @@
+// Karen Alexis R. De Guzman
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeGame : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
